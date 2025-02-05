80fbcb2fc0393e1f6b63e56213dabc2d628ec58e
jhoelito123 <amercer732@gmail.com>
2025-02-05 17:22:11 +0100

    Productos sectio

--- src/pages/productos/Productos.jsx
+++ src/pages/productos/Productos.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-
+import './Productos.css'
 const Productos = () => {
     return(
         <div className="productos-contenedor">
-            <hi>Productos en stock</hi>
+            <h1>Productos en stock</h1>
+            <button className="productos-btnAgregar">
+               (+) &emsp; Agregar nuevo
+            </button>
+            <div className="cardsProducto-contenedor">
+                
+            </div>
         </div>
     )
 }
