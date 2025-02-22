941e55b5e0a52f154b41d22bffac303b4fedde95
jhoelito123 <amercer732@gmail.com>
2025-02-22 20:35:47 +0100

    Order by modules, API and Routes

--- src/service/api.js
+++ src/service/api.js
@@ -38,8 +38,9 @@ export const addUser = (data) => api.post('/auth/sign-up', data);
 export const getAllClient = () => api.get('/clientes'); 
 export const deleteClient = (id) => api.delete(`/clientes/delete/${id}`);
 export const createClient = (data) => api.post('/clientes/create', data);
-export const fetchProductos = () => api.get('/productos-servicios');
+export const getUserVendor = () => api.get('/users/vendedores');
 
+export const fetchProductos = () => api.get('/productos-servicios');
 export const fetchPuntosDeVenta = () => api.get('/puntos-venta');
 export const fetchItems = () => api.get('/items');
 export const getItemID = (id) => api.get(`/items/${id}`);
@@ -47,40 +48,35 @@ export const createItem = (data) => api.post('/items', data);
 export const deleteItem = (id) => api.delete(`/items/${id}`);
 export const updateItem = (id, data) => api.put(`/items/${id}`, data);
 export const addCantidadItem = (id,data) => api.put(`/items/${id}/add/${data}`);
-
-export const getDocumentoIdentidad = () => api.get('/parametros/documentos-identidad');
+export const getProductoServicio = () => api.get('/productos-servicios');
 
 export const deleteUser = (id) => api.delete(`/users/${id}`);
 export const getRoles = () => api.get('/roles');
 export const getUsers = () => api.get('/users');
 export const updateUser = (id, data) => api.put(`/users/${id}`, data);
 export const getUserById = (id) => api.get(`/users/${id}`);
+
 export const getSucursales = () => api.get('/sucursales');
 
 export const getAllFacturas = () => api.get('/factura');
 export const emitirFactura = (data) => api.post('/factura/emitir', data);
 export const anularFactura = (data) => api.post('/factura/anular', data);
 export const revertirAnulacionFactura = (data) => api.post('/factura/reversion-anulacion', data);
-//emitir sin factura 
 export const emitirSinFactura = (data) => api.post('/ventas', data);
-//asd
 
 export const reservarProducto = (data) => api.post('/reserva', data);
-
 export const createDespacho = (data) => api.post('/despachos', data);
 
 export const createHoario = (data) => api.post('/horarios', data);
 export const getHorario = () => api.get('/horarios');
 
+//Estadisticas
 export const getItemsLimited = () => api.get('/items/limited');
 export const getClientLimited = () => api.get('/clientes/limited');
-//Estadisticas
 export const getStats = () => api.get('/stats');
-//vendedor
-export const getUserVendor = () => api.get('/users/vendedores');
 //parametros
 export const getTipoMoneda = () => api.get('/parametros/tipo-moneda');
 export const unidadesMedida = () => api.get('/parametros/unidades-medida');
 export const getUnidadMedida = () => api.get('/parametros/unidades-medida');
+export const getDocumentoIdentidad = () => api.get('/parametros/documentos-identidad');
 
-export const getProductoServicio = () => api.get('/productos-servicios');
