7e11f0d60d6b1c1da7f6d5af9296f935624fb96b
jhoelito123 <amercer732@gmail.com>
2025-02-24 22:12:06 +0100

    Style: close carrito modal

--- src/components/buttons/LinkButton.jsx
+++ src/components/buttons/LinkButton.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './LinkButton.css';
 
-const LinkButton = ({ to, children, className, ...props }) => {
+const LinkButton = ({ to, children, onClick, className, ...props }) => {
   return (
-    <Link to={to} className={`link-button ${className}`} {...props}>
+    <Link to={to} className={`link-button ${className}`} onClick={onClick} {...props}>
       {children}
     </Link>
   );
